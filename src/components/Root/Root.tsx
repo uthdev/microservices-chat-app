43236f771c1164276c0e2b17b4613cb425ed833d
uthdev <adelekegbolahan92@yahoo.com>
2022-01-08 18:46:32 +0100

    asetupreact  with  typescript and blueprint

---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Button, Intent } from '@blueprintjs/core'
+
+const Root = () => {
+  return (
+    <div>
+      <Button intent={Intent.SUCCESS} text="Click Me"/>
+    </div>
+  )
+}
+
+export default Root
